7a1e483a99677ebc1e5922fd5b6a4d631a19120f
luciana_hembert <contact@lucianahembert.com>
2017-08-04 08:14:46 +0200

    Entity Genus

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -10,7 +10,7 @@ namespace AppBundle\Controller;
 
 
 
-
+use AppBundle\Entity\Genus;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -21,6 +21,22 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class GenusController extends Controller
 {
+    /**
+     * @Route("/genus/new")
+     */
+    public function newAction()
+    {
+        $genus = new Genus();
+        $genus->setName('Octopus'.rand(1, 100));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($genus);
+        $em->flush();
+
+        return new Response('<html><body>Genus created!</body></html>');
+    }
+
+
     /**
      * @Route("/genus/{genusName}")
      */
